14668f4f9d97a6d5bc1d6252cb2f5b5ef1382d80
blakflagg <dduffy2@outlook.com>
2016-05-30 11:55:46 -0700

    basic web app demonstrating weather api using react

---
+++ app/components/About.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+
+
+var About = (props) => {
+  return(
+        <h3>About</h3>
+      )
+}
+
+module.exports = About;
---
+++ example-arrow-functions.js
@@ -0,0 +1,31 @@
+// var names = ['dan','julie','eric'];
+//
+// names.forEach(function(name){
+//   console.log('forEach ', name);
+// })
+//
+// names.forEach((name) =>{
+//     console.log('arrowFunc', name);
+// })
+//
+// names.forEach((name)=> console.log(name));
+//
+// var returnMe = (name) => name +'!';
+//
+// console.log(returnMe('Dan'));
+//
+// var person = {
+//   name: 'Dan',
+//   greet: function(){
+//     names.forEach((name) =>{
+//       console.log(this.name + ' says hi ' + name);
+//     });
+//
+//   }
+// };
+//
+// person.greet();
+
+var add = (a , b) => a+b;
+
+console.log(add(4,4));
